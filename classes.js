c145937442c6be41736a25abebacdd93a6a0d97e
Perlo1 <juancarlosmondejar6@gmail.com>
2023-11-05 12:58:03 +0100

    2

--- classes.js
+++ classes.js
@@ -1,21 +1,36 @@
 class Person {
   name; // NO ES NECESARIO
-  age; // NO ES NECESARIO
+  #age; // NO ES NECESARIO
   nationality; // NO ES NECESARIO
   static nationalityArr = ["US", "UK", "AUS", "CAN"];
 
   constructor(name, age, nationality) {
     this.name = name;
     if (typeof age === "number") {
-      this.age = age;
+      this.#age = age;
     } else {
-      this.age = Number(age);
+      this.#age = Number(age);
     }
     this.nationality = nationality;
   }
 
   static validNationality(nat = "") {
-    return Person.nationalityArr.reduce((prev, curr) => prev || curr === nat, false);
+    return Person.nationalityArr.reduce(
+      (prev, curr) => prev || curr === nat,
+      false
+    );
+  }
+  sayAge() {
+    return "I am" + " " +  this.#age + " " + "years old";
+  }
+  setAge(newVal) {
+    if (newVal < 1) {
+      this.#age = 1;
+    } else if (newVal > 100) {
+      this.#age = 110;
+    } else {
+      this.#age = newVal;
+    }
   }
 }
 
@@ -27,20 +42,20 @@ class Student extends Person {
 }
 
 class School {
-    country;
-    constructor(country) {
-        if(Person.validNationality(country)) {
-            this.country = country;
-        } else {
-            console.log("Invalid Country");
-        }
+  country;
+  constructor(country) {
+    if (Person.validNationality(country)) {
+      this.country = country;
+    } else {
+      console.log("Invalid Country");
     }
-
+  }
 }
 
 let greg = new Student("Greg", 21, "Spain", "English");
 // Prototype chain
 // Object -> Person -> Student
 
-console.log(greg.__proto__.__proto__);
 console.log(Person.validNationality("UK"));
+console.log(greg.sayAge());
+greg.age = 40;
